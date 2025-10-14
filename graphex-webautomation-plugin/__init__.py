e757686b67e5a3472580a138b533b959c355afac
Trevor Amburgey <tamburgey@mitre.org>
2025-10-14 10:48:54 -0400

    resolve more package hyphen issues

--- graphex-webautomation-plugin/__init__.py
+++ graphex-webautomation-plugin/__init__.py
@@ -1,6 +1,6 @@
 from graphex_webautomation_plugin import datatypes
 from graphex_webautomation_plugin import exceptions
-from graphex-webautomation-plugin.actions import playwright
+from graphex_webautomation_plugin.actions import playwright
 from graphex_webautomation_plugin import constants
 
 __all__ = [
